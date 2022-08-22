8ad377905820edb17d31d5537ca83245b7c7f20d
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-08-22 01:33:33 +0100

    UI profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://st2.depositphotos.com/1734074/11386/v/950/depositphotos_113862774-stock-illustration-vector-businessman-profile-icon-man.jpg" alt="profile image" class="rounded-circle" style="height: 150px;"/>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>username</h1></div>
+            <div class="d-flex">
+                <div class="px-4"><strong>999</strong> posts</div>
+                <div class="px-4"><strong>999</strong> followers</div>
+                <div class="px-4"><strong>999</strong> following</div>
             </div>
         </div>
     </div>
