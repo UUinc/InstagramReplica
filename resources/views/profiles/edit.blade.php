a5985c2c34e945afd725a619de8c8ad57a320ef2
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-08-24 00:45:21 +0100

    edit profile title, description and url

---
+++ resources/views/profiles/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<form action="/profile/{{ $user->id }}" enctype="multipart/form-data" method="POST">
+        @csrf
+        @method('PATCH')
+
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="row text-center">
+                    <h2>Edit Profile</h2>
+                </div>
+
+                <div class="form-group row">
+                    <label for="Title" class="col-md-4 col-form-label">Title</label>
+
+                    <input id="Title" 
+                        type="text" 
+                        class="form-control @error('Title') is-invalid @enderror" 
+                        name="Title" 
+                        value="{{ old('Title') ?? $user->profile->title }}" 
+                        required 
+                        autocomplete="Title" autofocus>
+
+                    @error('Title')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group row">
+                    <label for="Description" class="col-md-4 col-form-label">Description</label>
+
+                    <input id="Description" 
+                        type="text" 
+                        class="form-control @error('Description') is-invalid @enderror" 
+                        name="Description" 
+                        value="{{ old('Description') ?? $user->profile->description }}" 
+                        required 
+                        autocomplete="Description" autofocus>
+
+                    @error('Description')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="form-group row">
+                    <label for="Url" class="col-md-4 col-form-label">Url</label>
+
+                    <input id="Url" 
+                        type="text" 
+                        class="form-control @error('Url') is-invalid @enderror" 
+                        name="Url" 
+                        value="{{ old('Url') ?? $user->profile->url }}"
+                        autocomplete="Url" autofocus>
+
+                    @error('Url')
+                        <strong style="color: red;">{{ $message }}</strong>
+                    @enderror
+                </div>
+                
+                <div class="row">
+                    <label for="file" class="col-md-4 col-form-label">Profile Image</label>
+                    <input type="file" class="form-control-file" id="image" name="image"/>
+
+                    @error('image')
+                        <strong style="color: red;">{{ $message }}</strong>
+                    @enderror
+                </div>
+                
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,20 +7,30 @@
             <img src="/img/pic.jpg" alt="profile image" class="rounded-circle" style="height: 150px;"/>
         </div>
         <div class="col-8 pt-5">
-            <div class="ps-4 pb-3 d-flex justify-content-between">
+            <div class="ps-4 pb-3 d-flex ">
                 <h2 style="font-weight: lighter;">{{$user->username}}</h2>
-                <a href="/p/create">
+                <a 
+                href="/profile/{{ $user->id }}/edit" 
+                class="ms-4 mb-2 pt-1 ps-2 px-2" 
+                style=" color:black; 
+                        font-weight: bold; 
+                        border: 1px darkgray solid; 
+                        border-radius: 5px; 
+                        font-style:none;
+                        text-decoration: none;">
+                Edit profile</a>
+                <a href="/p/create" class="ps-4 pt-1">
                         <div><img src="/img/add.png" alt="add icon" style="height: 25px;"/></div>
                 </a>
             </div>
             <div class="d-flex pb-3">
                 <div class="px-4"><strong>{{$user->posts->count()}}</strong> posts</div>
-                <div class="px-4"><strong>1063</strong> followers</div>
-                <div class="px-4"><strong>708</strong> following</div>
+                <div class="px-4"><strong>1B</strong> followers</div>
+                <div class="px-4"><strong>0</strong> following</div>
             </div>
             <div class="ps-4"><strong>{{ $user->profile->title }}</strong></div>
             <div class="ps-4">{{ $user->profile->description }}</div>
-            <div class="ps-4"><a href="#">{{ $user->profile->url }}</a></div>
+            <div class="ps-4"><a href="{{ $user->profile->url }}">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
